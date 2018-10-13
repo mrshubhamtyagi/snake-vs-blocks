4417bf280f97c8714df8e667748046d863f9f380
Shubham Tyagi <styagi.2301@gmail.com>
2018-10-13 15:36:51 +0530

    Minor Implementation

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 targetOffset;
+    public float followSpeed = 0.4f;
+
+    void Start()
+    {
+        transform.position = targetOffset;
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = target.position + targetOffset;
+        targetPosition.x = 0f;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed);
+    }
+}
---
+++ Assets/Scripts/SnakeBody.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBody : MonoBehaviour
+{
+    public float turnSpeed = 10f;
+    public float moveSpeed = 10f;
+
+    private Vector3 position;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        position.x = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        position.y = moveSpeed * Time.deltaTime;
+        position.z = 0f;
+
+        Move();
+    }
+
+    private void Move()
+    {
+        position.x = Mathf.Clamp(position.x, -2.5f, 2.5f);
+        transform.Translate(position);
+    }
+}
